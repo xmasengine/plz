a80d85a7459775acda205c3dd0286bb750865ead
Xmasengine <xmasengine@gmail.com>
2026-05-10 22:27:25 +0900

    asm: Rework operands.

--- pkg/z80/asm/obj.go
+++ pkg/z80/asm/obj.go
@@ -1,5 +1,222 @@
 package asm
 
+type OperandKind int
+
+const (
+	KindNone OperandKind = iota
+	KindImm8
+	KindOff8
+	KindImm16
+	KindPtrImm8
+	KindPtrImm16
+	KindReg8
+	KindReg16
+	KindPtrReg8 // C can be contain pointer used for ports.
+	KindPtrReg16
+	KindRegSpc
+	KindFla // Flags
+	KindInt // Constant ints like for bit or imm, or for macros.
+	KindStr // For macros or other assembler instructions.
+)
+
+type Operand interface {
+	OperandKind() OperandKind
+}
+
+type OperandImm8 uint8
+
+func (OperandImm8) OperandKind() OperandKind {
+	return KindImm8
+}
+
+var _ Operand = OperandImm8(0)
+
+type OperandImm16 uint16
+
+func (OperandImm16) OperandKind() OperandKind {
+	return KindImm16
+}
+
+var _ Operand = OperandImm16(0)
+
+type OperandOff8 uint8
+
+func (OperandOff8) OperandKind() OperandKind {
+	return KindOff8
+}
+
+var _ Operand = OperandOff8(0)
+
+type OperandPtrImm8 uint8
+
+func (OperandPtrImm8) OperandKind() OperandKind {
+	return KindPtrImm8
+}
+
+var _ Operand = OperandPtrImm8(0)
+
+type OperandPtrImm16 uint16
+
+func (OperandPtrImm16) OperandKind() OperandKind {
+	return KindImm16
+}
+
+var _ Operand = OperandPtrImm16(0)
+
+type OperandReg8 int
+
+const (
+	OperandRegB OperandReg8 = 0
+	OperandRegC OperandReg8 = 1
+	OperandRegD OperandReg8 = 2
+	OperandRegE OperandReg8 = 3
+	OperandRegH OperandReg8 = 4
+	OperandRegL OperandReg8 = 5
+	OperandRegA OperandReg8 = 7 // Skip PtrHL as that is a pointer to reg 16.
+)
+
+func (OperandReg8) OperandKind() OperandKind {
+	return KindReg8
+}
+
+var _ Operand = OperandReg8(0)
+
+type OperandReg16 int
+
+const (
+	OperandRegBC OperandReg16 = 0
+	OperandRegDE OperandReg16 = 1
+	OperandRegHL OperandReg16 = 2
+	OperandRegAF OperandReg16 = 3
+	OperandRegSP OperandReg16 = 4
+	OperandRegIX OperandReg16 = 5
+	OperandRegIY OperandReg16 = 6
+)
+
+func (OperandReg16) OperandKind() OperandKind {
+	return KindReg16
+}
+
+var _ Operand = OperandReg16(0)
+
+type OperandRegSpc int
+
+const (
+	OperandRegI   OperandRegSpc = 0 // Interrupt register.
+	OperandRegR   OperandRegSpc = 1 // Memory refresh register can be read as random seed.
+	OperandRegAFS OperandRegSpc = 2 // AF shadow register pair.
+)
+
+func (OperandRegSpc) OperandKind() OperandKind {
+	return KindRegSpc
+}
+
+type OperandPtrReg8 int
+
+const (
+	OperandPtrRegC OperandPtrReg8 = 1 // Only posible for C which can be used as a pointer to a port.
+)
+
+func (OperandPtrReg8) OperandKind() OperandKind {
+	return KindPtrReg8
+}
+
+var _ Operand = OperandPtrReg8(0)
+
+type OperandPtrReg16 int
+
+const (
+	OperandPtrRegBC OperandPtrReg16 = 0
+	OperandPtrRegDE OperandPtrReg16 = 1
+	OperandPtrRegHL OperandPtrReg16 = 2
+	OperandPtrRegAF OperandPtrReg16 = 3
+	OperandPtrRegSP OperandPtrReg16 = 4
+	OperandPtrRegIX OperandPtrReg16 = 5
+	OperandPtrRegIY OperandPtrReg16 = 6
+)
+
+func (OperandPtrReg16) OperandKind() OperandKind {
+	return KindPtrReg16
+}
+
+var _ Operand = OperandPtrReg16(0)
+
+type OperandFla int
+
+const (
+	OperandFlaCarry      OperandFla = 7
+	OperandFlaMinus      OperandFla = 6
+	OperandFlaNoCarry    OperandFla = 5
+	OperandFlaNoZero     OperandFla = 4
+	OperandFlaParityEven OperandFla = 3
+	OperandFlaParityOdd  OperandFla = 2
+	OperandFlaParityZero OperandFla = 1
+)
+
+func (OperandFla) OperandKind() OperandKind {
+	return KindFla
+}
+
+var _ Operand = OperandFla(0)
+
+type OperandInt int
+
+func (OperandInt) OperandKind() OperandKind {
+	return KindInt
+}
+
+var _ Operand = OperandInt(0)
+
+type OperandString string
+
+func (OperandString) OperandKind() OperandKind {
+	return KindString
+}
+
+var _ Operand = OperandString(0)
+
+/*
+
+type Operand i
+
+	KindPtrBC     OperandKind = (1+iota)
+	KindPortPtrC
+	KindPtrDE
+	KindPtrHL
+	KindPtrIX
+	KindPtrIY
+	KindPtrImm8
+	KindPtrImm16
+	KindPtrSP
+	KindRegA
+	KindRegAF
+	KindRegAFS
+	KindRegB
+	KindRegBC
+	KindRegC
+	KindRegD
+	KindRegDE
+	KindRegE
+	KindRegH
+
+	KindRegHL
+	KindRegI
+	KindRegIX
+	KindRegIY
+	KindRegL
+	KindRegR
+	KindRegSP
+	KindFlag
+	KindImm8
+	KindOffset
+	KindImm16
+	KindReg
+	KindInt
+	KindString
+)
+
+
+
 type EntryKind int
 
 const (
@@ -34,6 +251,7 @@ type Operand interface {
 	Bytes() []byte
 	String() string
 }
+*/
 
 /* Possible operands:
 
@@ -71,6 +289,7 @@ and readability. For example:
 
 */
 
+/*
 type RegOperand byte
 type BitOperand byte
 type DispOperand uint8
@@ -180,3 +399,4 @@ func (o OperandInt) op() {}
 type OperandString string
 
 func (o OperandString) op() {}
+*/
